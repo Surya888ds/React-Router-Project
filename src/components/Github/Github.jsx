ead9f8b13e7e145957e5d6e5e041246fada639d9
Surya888ds <surya888ds@gmail.com>
2024-04-23 15:23:04 +0530

    Add New React Router project files

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github(){
+
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Surya888ds')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
+
+    const data = useLoaderData()
+
+    return (
+        <div className="text-center m-4 bg-gray-600
+        text-white p-4 text-3xl">
+            Github Repos: {data.public_repos}
+            <img src={data.avatar_url} alt="" width={300}/>
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Surya888ds')
+    return response.json();
+}
